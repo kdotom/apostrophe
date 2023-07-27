2d3032b46c78e275c7381f5998939381893a1940
kdotom <kopongmensah@gmail.com>
2023-07-27 10:15:44 -0400

    updated python files

--- python/simulator.py
+++ python/simulator.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 
 # read in yaml file
@@ -30,6 +30,9 @@ def get_unique_names(yaml_file):
 yaml_file = 'contracts.yaml'
 nodes = get_unique_names(yaml_file)
 
+
+# Load Contracts
+# -------------------------------------------
 with open(yaml_file, 'r') as file:
     contracts = yaml.safe_load(file)
 
@@ -45,21 +48,80 @@ def percentage_to_number(percentage):
     except ValueError:
         return None
 
-def calculate_payment(data_dict):
+# Calculate Remaining Balance
+# --------------------------------------
+
+def remaining_balance(principal, annual_interest_rate, monthly_payment, months_paid):
+    monthly_interest_rate = (annual_interest_rate) / 12  # Convert annual interest rate to a monthly rate
+    balance = principal
+
+    for _ in range(months_paid):
+        # Calculate the interest for the current month
+        interest = balance * monthly_interest_rate
+        
+        # Deduct the payment from the balance, taking into account the interest
+        balance = balance - (monthly_payment - interest)
+
+        # If balance is less than 0, set it to 0
+        balance = max(balance, 0)
+
+    return balance
+
+# print(remaining_balance(12000, 0.20, 200, 12))  # example calculation for interest-only
+
+# Calculate the number of months between two datetime objects
+# ---------------------------------------------------------------------
+def months_between(date1, date2):
+    # Ensure date1 is always the smaller (earlier) datetime
+    if date1 > date2:
+        date1, date2 = date2, date1
+
+    months = (date2.year - date1.year) * 12 + date2.month - date1.month
+
+    # If the day of the month of the earlier date is greater than the day of the month of the later date, subtract one month
+    if date1.day > date2.day:
+        months -= 1
+
+    return months
+
+
+
+# Calculate Payment
+# -------------------------------------------
+def calculate_payment(data_dict, date_prime):
     contract = data_dict.get("Contract")
 
     if contract == "Note":
+
         principal = float( data_dict.get("Principal") )
         note_type = data_dict.get("Note Type")
         interest = percentage_to_number( data_dict.get("Interest") )
-        if note_type == "Interest-Only" and interest is not None and principal is not None:
-            return (interest * principal) / 12
+
+        if note_type.lower() == "interest-only":
+            monthly_payment = (interest * principal) / 12
         else:
             monthly_payment = float( data_dict.get("Monthly Payment") )
+
+        contract_start_date =  datetime.strptime( data_dict.get('Start'), '%m/%d/%Y')
+        contract_end_date =  datetime.strptime( data_dict.get('End'), '%m/%d/%Y')
+        if date_prime ==  contract_end_date:
+            total_payments = months_between(contract_start_date, contract_end_date)
+            return remaining_balance(principal, interest, monthly_payment, total_payments)
+        elif (date_prime <= contract_end_date) and (date_prime >= contract_start_date):
             return monthly_payment
+        else:
+            return 0
     else:
-        monthly_payment = float( data_dict.get("Monthly Payment") )
-        return monthly_payment
+        contract_start_date =  datetime.strptime( data_dict.get('Start'), '%m/%d/%Y')
+        if data_dict.get('End') == None:
+            contract_end_date =  datetime.strptime( '12/31/9999', '%m/%d/%Y')
+        else:
+            contract_end_date =  datetime.strptime( data_dict.get('End'), '%m/%d/%Y')
+        if (date_prime < contract_end_date) and (date_prime > contract_start_date):
+            monthly_payment = float( data_dict.get("Monthly Payment") )
+            return monthly_payment
+        else:
+            return 0
 
     return None
 
@@ -81,23 +143,38 @@ date_prime = datetime.strptime(start_date_input, '%m/%d/%Y')
 
 import pandas as pd
 
+
+
+simulation_length_days = int( input('Simulation length (days):') )
+
 sources = []
 destinations = []
 monthly_payments = []
 
+
+# calculate cashflow:
+import calendar
+
 flow_data = []
 
-for contract in contracts:
-    sources.append(contract['Source'])
-    destinations.append(contract['Destination'])
-    monthly_payments.append( calculate_payment(contract) )
-    
-    flow_data.append( {'Source': contract['Source'], 'Destination': contract['Destination'], 'Monthly Payment': calculate_payment(contract) } )
+
+days_in_month = calendar.monthrange(date_prime.year, date_prime.month)[1]
+first_day_of_month = datetime(date_prime.year, date_prime.month, 1)
+for i in range(days_in_month):
+    current_date = first_day_of_month + timedelta(days=i)
+    for contract in contracts:
+        if contract['Payout Day'] == current_date.day:
+            sources.append(contract['Source'])
+            destinations.append(contract['Destination'])
+            monthly_payments.append( calculate_payment(contract, current_date) )
+        
+            flow_data.append( {'Source': contract['Source'], 'Destination': contract['Destination'], 'Monthly Payment': round( calculate_payment(contract, date_prime) , 2 ) } )
 
 
 data = {'Source': sources,
         'Destination': destinations,
-        'Monthly Payment': monthly_payments}
+        'Monthly Payment': monthly_payments,
+        }
 
 data = flow_data
 
@@ -112,40 +189,38 @@ for key, value in accounts.items():
 
 print('\n\n\n')
 
-for idx in range(365):
+
+
+account_state = [list(accounts.values())]
+
+for idx in range(simulation_length_days):
     for contract in contracts:
         if contract['Payout Day'] == date_prime.day:
-            #print('\n\n')
-            #print( contract['Source'] )
-            #print( contract['Destination'] )
-            #print( calculate_payment(contract) )
-            accounts[ contract['Source'] ] -= calculate_payment(contract)
-            accounts[ contract['Destination'] ] += calculate_payment(contract)
+            
+            accounts[ contract['Source'] ] -= calculate_payment(contract, date_prime)
+            accounts[ contract['Destination'] ] += calculate_payment(contract, date_prime)
+
+            account_state.append(list(accounts.values())) # note: verify that this appends in the right order (same as labels later)
 
     date_prime = date_prime + timedelta(days=1)
+
+account_state = np.transpose(account_state)
 
 print('Outputs')
 print('------------------')
 for key, value in accounts.items():
     print(f"{key}: {value:.2f}") 
 
+plt.figure()
+listed = list(accounts.keys())
+print(listed)
+idx = np.squeeze(np.where(np.array(list(accounts.keys())) == '8SC'))
+print('idx:'+str(idx))
+plt.plot(account_state[idx])
+plt.show()
 
 
-#### Generate data for online SanKey generator
-#### ----------------------------------------------------------------
-# this is for:
-# https://www.sankeymatic.com/build/
-def generate_text_file(yaml_file_path, output_file_path):
-    with open(yaml_file_path, 'r') as yaml_file:
-        data = yaml.safe_load(yaml_file)
 
-    with open(output_file_path, 'w') as output_file:
-        for entry in data:
-            source = entry['Source']
-            monthly_payment = entry['Monthly Payment']
-            destination = entry['Destination']
-            output_line = f"{source} [{monthly_payment:.2f}] {destination}\n"
-            output_file.write(output_line)
+#import subprocess
+#subprocess.run(["node", "../server.js"], check=True)
 
-
-generate_text_file('flow.yaml', 'flow.txt')
